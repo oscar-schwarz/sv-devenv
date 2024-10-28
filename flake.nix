9719aadb702b5727be15ba132580fb83f4f21138
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-10-28 21:24:20 +0100

    feat: add a simple flake to the repo that contains devenv

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very simple Nix flake that loads `devenv` into path when opening the devshell.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ pkgs.devenv ];
+          };
+        }
+      );
+}
