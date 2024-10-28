4280a5abff530d81abc09e1be6204e0da8ebdf28
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-10-28 21:41:27 +0100

    feat: add fetch script to flake

--- flake.nix
+++ flake.nix
@@ -11,7 +11,19 @@
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
           devShells.default = pkgs.mkShell {
-            buildInputs = [ pkgs.devenv ];
+            buildInputs = with pkgs; [ devenv curl ];
+
+            # Fetch the devenv.nix files if not present in repo
+            shellHook = ''
+              if ! [ -f "devenv.nix" ]; then
+                curl https://raw.githubusercontent.com/oscar-schwarz/devenv-beste-schule/refs/heads/main/devenv.lock \
+                  > devenv.lock && \
+                curl https://raw.githubusercontent.com/oscar-schwarz/devenv-beste-schule/refs/heads/main/devenv.nix \
+                  > devenv.nix && \
+                curl https://raw.githubusercontent.com/oscar-schwarz/devenv-beste-schule/refs/heads/main/devenv.yaml \
+                  > devenv.yaml
+              fi
+            '';
           };
         }
       );
